716741dbcc1cc0f044ba0d35206e6495e94b38a4
Dylan <meeusdylan@protonmail.com>
2020-07-11 01:25:08 +0200

    Add flags to parse input

--- wave/examples/amplitude/main.go
+++ wave/examples/amplitude/main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/DylanMeeus/Audio/wave/internal"
-	"os"
+)
+
+var (
+	input  = flag.String("i", "", "input file")
+	output = flag.String("o", "", "output file")
+	amp    = flag.Float64("a", 1.0, "amp mod factor")
 )
 
 func main() {
 	fmt.Println("Parsing wave file..")
-	args := os.Args
-	if len(args) < 1 {
-		panic("please provide a file..")
-	}
-	filename := args[1]
-	wave, err := internal.ReadWaveFile(filename)
+	flag.Parse()
+	infile := *input
+	outfile := *output
+	scale := *amp
+	wave, err := internal.ReadWaveFile(infile)
 	if err != nil {
 		panic("Could not parse wave file")
 	}
+
 	fmt.Printf("Read %v samples\n", len(wave.Samples))
 
 	// now try to write this file
-	scaledSamples := changeAmplitude(wave.Samples)
-	if err := internal.WriteSamples(scaledSamples, wave.WaveFmt, ""); err != nil {
+	scaledSamples := changeAmplitude(wave.Samples, scale)
+	if err := internal.WriteSamples(scaledSamples, wave.WaveFmt, outfile); err != nil {
 		panic(err)
 	}
 
 	fmt.Println("done")
 }
 
-func changeAmplitude(samples []internal.Sample) []internal.Sample {
+func changeAmplitude(samples []internal.Sample, scalefactor float64) []internal.Sample {
 	for i, s := range samples {
-		samples[i] = s * 4
+		samples[i] = internal.Sample(float64(s) * scalefactor)
 	}
 	return samples
 }
