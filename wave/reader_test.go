974c269b5bc41fc806bdfdc65b3ae146c3741634
Dylan <meeusdylan@protonmail.com>
2020-07-21 21:25:10 +0200

    Add test for reading wave files

--- wave/reader_test.go
+++ wave/reader_test.go
@@ -46,7 +46,37 @@ var (
 			-1,
 		},
 	}
+
+	// readFileTest to iterate over various files and make sure the output meets
+	// the expected Wave struct.
+	readFileTests = []struct {
+		file     string
+		expected Wave
+	}{}
 )
+
+// TestReadFile reads a golden file and ensures that is parsed as expected
+func TestReadFile(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fatalf("Should not have panic'd!\n%v", r)
+		}
+	}()
+	goldenfile := "./golden/maybe-next-time.wav"
+	wav, err := ReadWaveFile(goldenfile)
+	if err != nil {
+		t.Fatalf("Should be able to read wave file: %v", err)
+	}
+
+	// assert that the wav file looks as expected.
+	if wav.SampleRate != 44100 {
+		t.Fatalf("Expected SR 44100, got: %v", wav.SampleRate)
+	}
+
+	if wav.NumChannels != 2 {
+		t.Fatalf("Expected 2 channels, got: %v", wav.NumChannels)
+	}
+}
 
 func TestScaleFrames(t *testing.T) {
 	for _, test := range scaleFrameTests {
