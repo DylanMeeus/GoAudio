6da7668963c766e792c051ac200d5ab652a2b94b
Dylan <meeusdylan@protonmail.com>
2020-08-06 00:37:08 +0200

    update wave inspection tool

--- cmd/inspect/main.go
+++ cmd/inspect/main.go
@@ -5,14 +5,38 @@ package main
 import (
 	"flag"
 	"fmt"
-	wav "github.com/DylanMeeus/GoAudio/wave"
 	"os"
+
+	"github.com/DylanMeeus/GoAudio/wave"
+	wav "github.com/DylanMeeus/GoAudio/wave"
 )
 
 var (
 	input       = flag.String("i", "", "input file")
 	withSamples = flag.Bool("s", false, "with raw audio samples")
 )
+
+// printHeader
+func printHeader(h wave.WaveHeader) {
+	fmt.Println("Header")
+	fmt.Printf("Chunk ID: %v\n", string(h.ChunkID))
+	fmt.Printf("Chunk Size: %v\n", h.ChunkSize)
+	fmt.Printf("Format: %v\n", string(h.Format))
+
+}
+
+func printFormat(f wave.WaveFmt) {
+	fmt.Println("Wave FMT")
+	fmt.Printf("SubchunkID: %v\n", string(f.Subchunk1ID))
+	fmt.Printf("SubchunkSize: %v\n", f.Subchunk1Size)
+	fmt.Printf("AudioFormat: %v\n", f.AudioFormat)
+	fmt.Printf("Channels: %v\n", f.NumChannels)
+	fmt.Printf("SampleRate: %v\n", f.SampleRate)
+	fmt.Printf("ByteRate: %v\n", f.ByteRate)
+	fmt.Printf("BlockAlign: %v\n", f.BlockAlign)
+	fmt.Printf("BitsPerSample: %v\n", f.BitsPerSample)
+
+}
 
 func main() {
 	flag.Parse()
@@ -27,11 +51,9 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Header:\n")
-	fmt.Printf("%+v\n", wave.WaveHeader)
+	printHeader(wave.WaveHeader)
 	fmt.Printf("===============\n")
-	fmt.Printf("Fmt:\n")
-	fmt.Printf("%+v\n", wave.WaveFmt)
+	printFormat(wave.WaveFmt)
 	if ws {
 		fmt.Printf("===============\nSamples:\n")
 		fmt.Printf("%v\n", wave.RawData)
