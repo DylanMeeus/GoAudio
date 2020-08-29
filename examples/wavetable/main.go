61cb2bff23ad6b18d0e5d935100d1064e31f5df9
Dylan <meeusdylan@protonmail.com>
2020-08-29 16:19:17 +0200

    Pass the desired shape as a program flag

--- examples/wavetable/main.go
+++ examples/wavetable/main.go
@@ -10,19 +10,31 @@ import (
 // example use of the oscillator to generate different waveforms
 var (
 	duration = flag.Int("d", 10, "duration of signal in seconds")
+	shape    = flag.String("s", "square", "waveshape to generate")
 	amp      = flag.Float64("a", 1, "Amplitude of signal")
 	harms    = flag.Int("h", 1, "Number of harmonics of signal")
 	freq     = flag.Float64("f", 440, "Frequency of signal")
 	output   = flag.String("o", "", "output file")
 )
 
+var (
+	shapefunc = map[string]func(int, int) []float64{
+		"triangle": synth.TriangleTable,
+		"square":   synth.SquareTable,
+		"saw":      synth.SawTable,
+	}
+)
+
 func main() {
-	fmt.Println("Generating triangle wave")
 	flag.Parse()
 
 	wfmt := wave.NewWaveFmt(1, 1, 44100, 16, nil)
-	triangles := synth.TriangleTable(*harms, *duration*wfmt.SampleRate)
-	table := synth.NewGtable(triangles)
+
+	waveform := shapefunc[*shape]
+	if waveform == nil {
+		waveform = synth.SquareTable
+	}
+	table := synth.NewGtable(waveform(*harms, *duration*wfmt.SampleRate))
 
 	osc, err := synth.NewLookupOscillator(wfmt.SampleRate, table, 0.0)
 	if err != nil {
@@ -37,5 +49,5 @@ func main() {
 	if err := wave.WriteFrames(frames, wfmt, *output); err != nil {
 		panic(err)
 	}
-
+	fmt.Println("done")
 }
