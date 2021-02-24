ea10f6997379c86942956a5e3215a0552d2e1ef9
Dylan <meeusdylan@protonmail.com>
2021-02-24 20:13:53 +0100

    add FFT/HFFT implementations

--- math/dft.go
+++ math/dft.go
@@ -1,8 +1,9 @@
-package spectral
+package math
 
 import (
 	"github.com/DylanMeeus/GoAudio/wave"
 	"math"
+	"math/cmplx"
 )
 
 const (
@@ -10,6 +11,8 @@ const (
 )
 
 // DFT is a discrete fourier transformation on the input frames
+// DEPRECATED
+// Please use FFT unless you are sure  you want this one..
 func DFT(input []wave.Frame) []complex128 {
 	N := len(input)
 
@@ -32,4 +35,30 @@ func DFT(input []wave.Frame) []complex128 {
 	}
 
 	return output
+}
+
+// HFFT mutates freqs!
+func HFFT(input []wave.Frame, freqs []complex128, n, step int) {
+	if n == 1 {
+		freqs[0] = complex(input[0], 0)
+		return
+	}
+
+	h := n / 2
+
+	HFFT(input, freqs, h, 2*step)
+	HFFT(input[step:], freqs[step:], h, 2*step)
+
+	for k := 0; k < h; k++ {
+		a := -2 * math.Pi * float64(k) * float64(n)
+		e := cmplx.Rect(1, a) * freqs[k+h]
+		freqs[k], freqs[k+h] = freqs[k]+e, freqs[k]-e
+	}
+}
+
+// FFT (Fast Fourier Transform) implementation
+func FFT(input []wave.Frame) []complex128 {
+	freqs := make([]complex128, len(input))
+	HFFT(input, freqs, len(input), 1)
+	return freqs
 }
